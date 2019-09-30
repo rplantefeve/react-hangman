b22b9133fc1cd9ee30b99be0610899fbc0699c1b
rplantefeve <romain.plantefeve@gmail.com>
2019-09-30 12:18:16 +0200

    Jeu fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import shuffle from 'lodash.shuffle';
+
+import Button from './Button';
+
+const MOTS = [
+  'synallagmatique',
+  'reactance',
+  'simulacre',
+  'roquette',
+  'flexueux',
+  'pugilat',
+  'trieuseensacheuse',
+  'thalassotherapie',
+  'incipit',
+  'anticonstitutionnellement',
+  'hyperpresidentialisation',
+  'supercalifragilisticexpialidocious',
+  'dichlorodiphenyltrichloroethane'
+];
+
+const ALPHABET = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mots: this.shuffleWords(),
+      usedLetters: new Set(),
+      position: 0,
+    }
+  }
+
+  // Mélange les mots
+  shuffleWords() {
+    return shuffle(MOTS);
+  }
+
+  getWordFromList(position){
+    return this.state.mots[position];
+  }
+
+  // Produit une représentation textuelle de l’état de la partie,
+  // chaque lettre non découverte étant représentée par un _underscore_.
+  // (CSS assurera de l’espacement entre les lettres pour mieux
+  // visualiser le tout).
+  computeDisplay(phrase, usedLetters) {
+    return phrase.replace(/\w/g, (letter) => (
+      usedLetters.has(letter)
+      ? letter
+      : '_'))
+  }
+
+  handleClick = index => {
+    // ajout de la lettre aux lettres utilisées
+    console.log(index);
+    this.setState({usedLetters: this.state.usedLetters.add(ALPHABET[index])});
+  }
+
+  handleKeyVisibility(letter){
+    if(this.state.usedLetters.has(letter)){
+      return ' disabled';
+    } else {
+      return '';
+    }
+  }
+
+  render() {
+    const mot = this.getWordFromList(this.state.position);
+    let width = (mot.length + 1)* 9;
+    return (
+      <div className="App">
+        <div>
+        <input className="App-input" type="text" style={{width:`${width}px`}} value={this.computeDisplay(mot, this.state.usedLetters)} readOnly/>
+        </div>
+        <div className="App-keyboard">
+        {ALPHABET.map((letter, index) => (
+          <Button key={index} onClick={this.handleClick} letter={letter} index={index} visibility={this.handleKeyVisibility(letter)}/>
+        ))}
+        </div>
+    </div>);
+  }
+
 }
+
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ({letter, index, onClick, visibility}) => {
+  let className = "App-button" + visibility;
+  return (
+    <button className={className}
+      onClick={() => onClick(index)}
+    >{letter}</button>
+  )
+}
+
+Button.propTypes = {
+  letter: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Button
